eb0eb25a45fd2daec0f8f0e39a0d3d637221dbe0
yanggggjie <60206592+yanggggjie@users.noreply.github.com>
2024-01-07 21:02:56 +0800

    feature when change language filter scroll to top

--- components/rank/RankTable.tsx
+++ components/rank/RankTable.tsx
@@ -2,6 +2,7 @@
 import { clsx } from 'clsx'
 import dayjs from 'dayjs'
 import {
+  ColumnFiltersState,
   flexRender,
   getCoreRowModel,
   getFilteredRowModel,
@@ -18,24 +19,38 @@ import {
 import * as React from 'react'
 import { createColumns } from '@/components/rank/createColumns'
 import { IRankItem } from '@/server-actions/kv/setRank'
+import { useRef } from 'react'
 interface Props {
   data: IRankItem[]
 }
 
 export default function RankTable({ data }: Props) {
   const columns = createColumns(data)
-
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    [],
+  )
+  const tableRef = useRef(null)
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      columnFilters,
+    },
+    onColumnFiltersChange: (updaterOrValue) => {
+      setColumnFilters(updaterOrValue)
+      if (tableRef.current) {
+        // @ts-ignore
+        tableRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' })
+      }
+    },
   })
 
   return (
     <div className={clsx('m-4')}>
       <Table className={clsx('block overflow-auto h-[900px]', 'border-2')}>
-        <TableHeader className={clsx('sticky top-0 bg-white z-10')}>
+        <TableHeader className={clsx('block sticky top-0 bg-white z-10')}>
           {table.getHeaderGroups().map((headerGroup) => {
             return (
               <TableRow key={headerGroup.id}>
@@ -53,7 +68,12 @@ export default function RankTable({ data }: Props) {
             )
           })}
         </TableHeader>
-        <TableBody>
+        <TableBody
+          ref={tableRef}
+          style={{
+            scrollMarginTop: '100px',
+          }}
+        >
           {table.getRowModel().rows.map((row) => {
             return (
               <TableRow key={row.id}>

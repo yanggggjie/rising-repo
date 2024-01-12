c187e5ad434d2a0e90967aba2599d29e61d1a440
yanggggjie <60206592+yanggggjie@users.noreply.github.com>
2024-01-12 17:10:07 +0800

    run out vercel Analytics run out, remove it

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
-import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export const metadata: Metadata = {
@@ -19,7 +18,6 @@ export default async function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
-        <Analytics></Analytics>
         <SpeedInsights />
       </body>
     </html>
